e8a4b37ec510a7d2dcad6b5d0a4da8049ce9c582
Mohammad Shakhatreh <hmodaa.sha@hotmail.com>
2017-12-23 09:42:26 +0200

    some changes

--- src/JTranslator.java
+++ src/JTranslator.java
@@ -1,5 +1,8 @@
 import com.translator.Translator;
 
+//this class has been made as workaround for linux
+//because the of the application is the main class
+//the before this class is com-translator-Translator
 public class JTranslator {
 
     //main method to start the application from
