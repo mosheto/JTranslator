f9017d1728fc72d1ed3458d7d5911e257d7d7cd4
Mohammad Shakhatreh <hmodaa.sha@hotmail.com>
2017-12-23 09:25:11 +0200

    change app intry point and remove system LaF and add About.form and changed the name of the app

---
+++ src/JTranslator.java
@@ -0,0 +1,10 @@
+import com.translator.Translator;
+
+
+public class JTranslator {
+
+    //main method to start the application from
+    public static void main(String[] args) {
+        new Translator();
+    }
+}
